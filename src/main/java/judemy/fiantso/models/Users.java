38d945422341d55a7984461a3ebae3cb1f83adb8
Fiantso Harena <fiantsorav@gmail.com>
2023-08-19 20:34:03 +0300

    fix: Fixing a bug by adding the NoArgsConstructor annotation

--- src/main/java/judemy/fiantso/models/Users.java
+++ src/main/java/judemy/fiantso/models/Users.java
@@ -1,8 +1,12 @@
 package judemy.fiantso.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Users {
     private Long userId;
     private String name;

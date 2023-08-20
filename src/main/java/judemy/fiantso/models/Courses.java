b62cecf2e48f08431e7f84bcc222bfc508db80de
Fiantso Harena <fiantsorav@gmail.com>
2023-08-20 19:56:43 +0300

    chore: Added the AllArgsConstructor and NoArgsConstructor lombok annotation

--- src/main/java/judemy/fiantso/models/Courses.java
+++ src/main/java/judemy/fiantso/models/Courses.java
@@ -1,8 +1,12 @@
 package judemy.fiantso.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Courses {
     private Long courseId;
     private String title;

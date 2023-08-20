b0d498c23a0aebe99ae8138f3f8c86f2cbd582bb
Fiantso Harena <fiantsorav@gmail.com>
2023-08-20 20:34:10 +0300

    feat: Create a controller for course DAO

--- src/main/java/judemy/fiantso/controller/CourseController.java
+++ src/main/java/judemy/fiantso/controller/CourseController.java
@@ -1,4 +1,55 @@
 package judemy.fiantso.controller;
 
+import judemy.fiantso.models.Courses;
+import judemy.fiantso.service.courseService.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Courses> createCourse(@RequestBody Courses course) {
+        Courses createdCourse = courseService.createCourse(course);
+        if (createdCourse != null) {
+            return ResponseEntity.ok(createdCourse);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Courses>> getAllCourses() {
+        List<Courses> users = courseService.getAllCourses();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Courses> getCourseById(@PathVariable Long courseId) {
+        Courses course = courseService.getCourseById(courseId);
+        if (course != null) {
+            return ResponseEntity.ok(course);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{courseId}")
+    public Courses updateCourse(@PathVariable Long courseId, @RequestBody Courses course) {
+        course.setCourseId(courseId);
+        return this.courseService.updateCourse(course);
+    }
+
+    @DeleteMapping("/{courseId}")
+    public void deleteCourse(@PathVariable Long courseId) {
+        this.courseService.deleteCourse(courseId);
+    }
 }

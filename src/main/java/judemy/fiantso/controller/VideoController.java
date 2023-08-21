ad26678e19a622b86611f9033953988935416624
Fiantso Harena <fiantsorav@gmail.com>
2023-08-21 14:46:01 +0300

    feat: Finish the implementation of the video DAO by making a controller

--- src/main/java/judemy/fiantso/controller/VideoController.java
+++ src/main/java/judemy/fiantso/controller/VideoController.java
@@ -1,2 +1,45 @@
-package judemy.fiantso.controller;public class VideoController {
+package judemy.fiantso.controller;
+
+import judemy.fiantso.models.Videos;
+import judemy.fiantso.service.VideoService.VideoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/videos")
+public class VideoController {
+    private final VideoService videoService;
+
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Videos> createVideo(@RequestBody Videos video) {
+        Videos createdVideo = videoService.createVideo(video);
+        if (createdVideo != null) {
+            return ResponseEntity.ok(createdVideo);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Videos>> getAllVideos() {
+        List<Videos> videos = videoService.getAllVideos();
+        return ResponseEntity.ok(videos);
+    }
+
+    @PutMapping("/{videoId}")
+    public Videos updateVideo(@PathVariable Long videoId, @RequestBody Videos video) {
+        video.setVideoId(videoId);
+        return this.videoService.updateVideo(video);
+    }
+
+    @DeleteMapping("/{videoId}")
+    public void deleteVideo(@PathVariable Long videoId) {
+        this.videoService.deleteVideo(videoId);
+    }
 }

7d6776b90fe8f1cbeb60d118926ea757a02e1900
Fiantso Harena <fiantsorav@gmail.com>
2023-08-21 09:28:55 +0300

    fix: Change type of enrollment_date localdatetime to date

--- src/main/java/judemy/fiantso/models/Enrollments.java
+++ src/main/java/judemy/fiantso/models/Enrollments.java
@@ -1,14 +1,20 @@
 package judemy.fiantso.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Enrollments {
     private Long enrollmentId;
-    private Users user;
-    private Courses course;
-    private LocalDateTime enrollmentDate;
+    private int userId;
+    private int courseId;
+    private Date enrollmentDate;
     private String status;
+
 }

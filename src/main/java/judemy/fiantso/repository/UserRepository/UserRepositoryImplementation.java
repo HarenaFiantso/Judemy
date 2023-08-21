85be1a8a22832dba0b3b91ada684b3bbb09cac74
Fiantso Harena <fiantsorav@gmail.com>
2023-08-21 17:56:09 +0300

    fix: Change the error handler by using slf4j instead of a simple system.out.println

--- src/main/java/judemy/fiantso/repository/UserRepository/UserRepositoryImplementation.java
+++ src/main/java/judemy/fiantso/repository/UserRepository/UserRepositoryImplementation.java
@@ -3,16 +3,28 @@ package judemy.fiantso.repository.UserRepository;
 import judemy.fiantso.models.Users;
 import judemy.fiantso.repository.JudemyRepository;
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Repository
 public class UserRepositoryImplementation implements JudemyRepository<Users> {
+    private static final Logger logger = LoggerFactory.getLogger(UserRepositoryImplementation.class);
+
     private final Connection connection;
+    private static final String INSERT_QUERY = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    private static final String SELECT_BY_ID_QUERY = "SELECT * FROM users WHERE user_id = ?";
+    private static final String SELECT_ALL_QUERY = "SELECT * FROM users";
+    private static final String UPDATE_QUERY = "UPDATE users SET name = ?, email = ?, password = ? WHERE user_id = ?";
+    private static final String DELETE_QUERY = "DELETE FROM users WHERE user_id = ?";
 
     public UserRepositoryImplementation(Connection connection) {
         this.connection = connection;
@@ -20,40 +32,32 @@ public class UserRepositoryImplementation implements JudemyRepository<Users> {
 
     @Override
     public Users create(Users user) {
-        String insertQuery = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-
-        try (PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_QUERY)) {
             statement.setString(1, user.getName());
             statement.setString(2, user.getEmail());
             statement.setString(3, user.getPassword());
 
             statement.executeUpdate();
-            System.out.println("The data insert query is executed successfully ! ");
+            logger.info("Data insert query executed successfully!");
         } catch (SQLException e) {
-            System.out.println("There is an error while executing the insert query : " + e.getMessage());
+            logger.error("Error executing insert query: {}", e.getMessage());
         }
         return user;
     }
 
     @Override
     public Users getById(Long id) {
-        String selectQuery = "SELECT * FROM users WHERE user_id = ?";
         Users user = new Users();
 
-        try (PreparedStatement statement = connection.prepareStatement(selectQuery)) {
+        try (PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID_QUERY)) {
             statement.setLong(1, id);
             ResultSet resultSet = statement.executeQuery();
             resultSet.next();
 
-            user = new Users(
-                    resultSet.getLong("user_id"),
-                    resultSet.getString("name"),
-                    resultSet.getString("email"),
-                    resultSet.getString("password")
-            );
-            System.out.println("The data select id query is executed successfully !");
+            user = new Users(resultSet.getLong("user_id"), resultSet.getString("name"), resultSet.getString("email"), resultSet.getString("password"));
+            logger.info("Data select by id query executed successfully!");
         } catch (SQLException e) {
-            System.out.println("There is an error while executing the select by id query : " + e.getMessage());
+            logger.error("Error executing select by id query: {}", e.getMessage());
         }
 
         return user;
@@ -61,23 +65,17 @@ public class UserRepositoryImplementation implements JudemyRepository<Users> {
 
     @Override
     public List<Users> getAll() {
-        String selectQuery = "SELECT * FROM users";
         List<Users> users = new ArrayList<>();
 
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(selectQuery);
+        try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL_QUERY)) {
+            ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
-                users.add(new Users(
-                        resultSet.getLong("user_id"),
-                        resultSet.getString("name"),
-                        resultSet.getString("email"),
-                        resultSet.getString("password")
-                ));
+                users.add(new Users(resultSet.getLong("user_id"), resultSet.getString("name"), resultSet.getString("email"), resultSet.getString("password")));
             }
-            System.out.println("The data select query is executed successfully !");
+            logger.info("Data select query executed successfully!");
         } catch (SQLException e) {
-            System.out.println("There is an error while executing the select query : " + e.getMessage());
+            logger.error("Error executing select query: {}", e.getMessage());
         }
 
         return users;
@@ -85,33 +83,28 @@ public class UserRepositoryImplementation implements JudemyRepository<Users> {
 
     @Override
     public Users update(Users user) {
-        String updateQuery = "UPDATE users SET name = ?, email = ?, password = ? WHERE user_id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(updateQuery)) {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)) {
             statement.setString(1, user.getName());
             statement.setString(2, user.getEmail());
             statement.setString(3, user.getPassword());
             statement.setLong(4, user.getUserId());
 
             statement.executeUpdate();
-            System.out.println("The data update query is executed successfully !");
+            logger.info("Data update query executed successfully!");
         } catch (SQLException e) {
-            System.out.println("There is an error while executing the update query : " + e.getMessage());
+            logger.error("Error executing update query: {}", e.getMessage());
         }
 
         return user;
     }
 
     public void delete(Long id) {
-        String deleteQuery = "DELETE FROM users WHERE user_id = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(deleteQuery)) {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)) {
             statement.setLong(1, id);
             statement.executeUpdate();
-            System.out.println("The data delete query is executed successfully !");
+            logger.info("Data delete query executed successfully!");
         } catch (SQLException e) {
-            System.out.println("There is an error while executing the delete query : " + e.getMessage());
+            logger.error("Error executing delete query: {}", e.getMessage());
         }
     }
-
 }

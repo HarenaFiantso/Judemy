64e346e3772feb4c0b677e6f5062e5749d775914
Fiantso Harena <fiantsorav@gmail.com>
2023-08-21 09:29:58 +0300

    feat: Implement the enrollment service

--- src/main/java/judemy/fiantso/service/EnrolllmentService/EnrollmentService.java
+++ src/main/java/judemy/fiantso/service/EnrolllmentService/EnrollmentService.java
@@ -1,2 +1,17 @@
-package judemy.fiantso.service.EnrolllmentService;public interface EnrollmentService {
+package judemy.fiantso.service.EnrolllmentService;
+
+import judemy.fiantso.models.Enrollments;
+
+import java.util.List;
+
+public interface EnrollmentService {
+    Enrollments createEnrollment(Enrollments enrollment);
+
+    List<Enrollments> getAllEnrollments();
+
+    Enrollments getEnrollmentById(Long enrollmentId);
+
+    Enrollments updateEnrollment(Enrollments enrollment);
+
+    void deleteEnrollment(Long enrollment);
 }
--- src/main/java/judemy/fiantso/service/EnrolllmentService/EnrollmentServiceImplementation.java
+++ src/main/java/judemy/fiantso/service/EnrolllmentService/EnrollmentServiceImplementation.java
@@ -1,2 +1,41 @@
-package judemy.fiantso.service.EnrolllmentService;public class EnrollmentServiceImplementation {
+package judemy.fiantso.service.EnrolllmentService;
+
+import judemy.fiantso.models.Enrollments;
+import judemy.fiantso.repository.JudemyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnrollmentServiceImplementation implements EnrollmentService {
+    private final JudemyRepository<Enrollments> enrollmentRepository;
+
+    public EnrollmentServiceImplementation(JudemyRepository<Enrollments> enrollmentRepository) {
+        this.enrollmentRepository = enrollmentRepository;
+    }
+
+    @Override
+    public Enrollments createEnrollment(Enrollments enrollment) {
+        return enrollmentRepository.create(enrollment);
+    }
+
+    @Override
+    public List<Enrollments> getAllEnrollments() {
+        return enrollmentRepository.getAll();
+    }
+
+    @Override
+    public Enrollments getEnrollmentById(Long enrollmentId) {
+        return enrollmentRepository.getById(enrollmentId);
+    }
+
+    @Override
+    public Enrollments updateEnrollment(Enrollments enrollment) {
+        return enrollmentRepository.update(enrollment);
+    }
+
+    @Override
+    public void deleteEnrollment(Long enrollment) {
+        enrollmentRepository.delete(enrollment);
+    }
 }

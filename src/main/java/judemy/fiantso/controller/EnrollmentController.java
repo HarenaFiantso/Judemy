3b4f0088ed659b040ab870be535d3c8967b6c810
Fiantso Harena <fiantsorav@gmail.com>
2023-08-21 09:30:54 +0300

    feat: Finish the implementation of the enrollment entity DAO by creating a controller

--- src/main/java/judemy/fiantso/controller/EnrollmentController.java
+++ src/main/java/judemy/fiantso/controller/EnrollmentController.java
@@ -1,2 +1,55 @@
-package judemy.fiantso.controller;public class EnrollmentController {
+package judemy.fiantso.controller;
+
+import judemy.fiantso.models.Enrollments;
+import judemy.fiantso.service.EnrolllmentService.EnrollmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/enrollments")
+public class EnrollmentController {
+    private final EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Enrollments> createEnrollment(@RequestBody Enrollments enrollment) {
+        Enrollments createdEnrollment = enrollmentService.createEnrollment(enrollment);
+        if (createdEnrollment != null) {
+            return ResponseEntity.ok(createdEnrollment);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Enrollments>> getAllEnrollment() {
+        List<Enrollments> enrollments = enrollmentService.getAllEnrollments();
+        return ResponseEntity.ok(enrollments);
+    }
+
+    @GetMapping("/{enrollmentId}")
+    public ResponseEntity<Enrollments> getEnrollmentById(@PathVariable Long enrollmentId) {
+        Enrollments enrollment = enrollmentService.getEnrollmentById(enrollmentId);
+        if (enrollment != null) {
+            return ResponseEntity.ok(enrollment);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{enrollmentId}")
+    public Enrollments updateEnrollment(@PathVariable Long enrollmentId, @RequestBody Enrollments enrollment) {
+        enrollment.setEnrollmentId(enrollmentId);
+        return this.enrollmentService.updateEnrollment(enrollment);
+    }
+
+    @DeleteMapping("/{enrollmentId}")
+    public void deleteUser(@PathVariable Long enrollmentId) {
+        this.enrollmentService.deleteEnrollment(enrollmentId);
+    }
 }

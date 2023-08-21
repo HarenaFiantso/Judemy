b845e67b258e97ecb308574684426428e8b5b9f9
Fiantso Harena <fiantsorav@gmail.com>
2023-08-21 11:24:04 +0300

    feat: Initialize the implementation of the lesson repository

--- src/main/java/judemy/fiantso/service/LessonService/LessonRepositoryImplementation.java
+++ src/main/java/judemy/fiantso/service/LessonService/LessonRepositoryImplementation.java
@@ -1,4 +1,39 @@
 package judemy.fiantso.service.LessonService;
 
-public class LessonRepositoryImplementation {
+import judemy.fiantso.models.Lessons;
+import judemy.fiantso.repository.JudemyRepository;
+
+import java.util.List;
+
+public class LessonRepositoryImplementation implements LessonRepository {
+    private final JudemyRepository<Lessons> lessonRepository;
+
+    public LessonRepositoryImplementation(JudemyRepository<Lessons> lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Override
+    public Lessons createLesson(Lessons lesson) {
+        return null;
+    }
+
+    @Override
+    public List<Lessons> getAllLessons() {
+        return null;
+    }
+
+    @Override
+    public Lessons getLessonById(Long lessonId) {
+        return null;
+    }
+
+    @Override
+    public Lessons updateLesson(Lessons lesson) {
+        return null;
+    }
+
+    @Override
+    public void deleteLesson(Long lesson) {
+
+    }
 }

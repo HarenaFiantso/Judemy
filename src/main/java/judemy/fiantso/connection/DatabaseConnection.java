ad94909f2cf00a0809f521b19c70e32026643ee5
Fiantso Harena <fiantsorav@gmail.com>
2023-08-22 12:33:23 +0300

    fix: Delete the database connection test

--- src/main/java/judemy/fiantso/connection/DatabaseConnection.java
+++ src/main/java/judemy/fiantso/connection/DatabaseConnection.java
@@ -1,19 +1,32 @@
 package judemy.fiantso.connection;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Configuration
 public class DatabaseConnection {
+    private static HikariDataSource dataSource;
 
-    private HikariDataSource dataSource;
 
-    public DatabaseConnection(HikariDataSource dataSource) {
-        this.dataSource = dataSource;
+    public DatabaseConnection() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(Credentials.DATABASE_URL);
+        config.setUsername(Credentials.DATABASE_USER);
+        config.setPassword(Credentials.DATABASE_PASSWORD);
+
+        dataSource = new HikariDataSource(config);
     }
 
-    public Connection getConnection() throws SQLException {
+    @Bean
+    public static Connection getConnection() throws SQLException {
+        if (dataSource == null) {
+            new DatabaseConnection();
+        }
         return dataSource.getConnection();
     }
 }
